d4fd92a36e1aa58c722a489e295d1461c9ed0b1a
Igor Lopes <igor@nucleus.eti.br>
2018-07-28 17:59:03 -0300

    Setup Slim PHP

---
+++ bootstrap/app.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App([
+    'displayErrorDetails' => true
+]);
+
+require __DIR__ . '/../app/routes.php';
