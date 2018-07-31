70f95699f0d283db9b8f6abbc4915e8c79e810d2
Igor Lopes <igor@nucleus.eti.br>
2018-07-31 20:14:02 -0300

    Setup eloquent

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,6 +8,13 @@ class HomeController extends Controller
 {
     public function index($req, $res)
     {
+        $this->db->schema()->create('users', function ($table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+
+        die();
         return $this->view->render($res, 'home.twig');
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,31 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Slim\App([
     'settings' => [
-        'displayErrorDetails' => true
+        'displayErrorDetails' => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost:8889',
+            'database' => 'slimstarter',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => ''
+         ]
     ]
 ]);
 
 $container = $app ->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function($container) use ($capsule){
+    
+    return $capsule;
+};
 
 $container['view'] = function($container){
 
