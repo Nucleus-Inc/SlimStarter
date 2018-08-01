e51966d5f2e7e347d415bb7173439653b2b72962
Igor Lopes <igor@nucleus.eti.br>
2018-07-31 22:56:54 -0300

    Add user signup

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers\Auth;
 
+use App\Models\User;
+
 use App\Controllers\Controller;
 
 class AuthController extends Controller
@@ -13,6 +15,10 @@ class AuthController extends Controller
 
     public function postSignUp($req, $res)
     {
-        return '';
+        User::create([
+            'email' => $req->getParam('email'),
+            'name' => $req->getParam('name'),
+            'password' => password_hash(hash('sha512', $req->getParam('password')), PASSWORD_BCRYPT)
+        ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,5 +9,7 @@ class User extends Model
 
 {
     protected $table = 'users';
+
+    protected $fillable = ['email', 'name', 'password'];
     
 }
